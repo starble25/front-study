61f2b9a17161f161cdf92a45e53327b47b25cef2
starble25 <starble25@gmail.com>
2024-10-15 14:58:05 +0900

    javascript 기초 맛보기

---
+++ javascript/basic.js
@@ -0,0 +1,3 @@
+function out_btn_click(){
+    console.log('버튼 눌림. 외부 스크립트 실행');
+}
---
+++ javascript/basic01.js
@@ -0,0 +1,15 @@
+
+// 변수 (let, const)
+
+let a = 10; // a라는 변수를 선언하고 그 안에 값 10을 저장한다
+let b; // b 변수 선언
+
+b = 15; // b에 15라는 값을 저장(할당)한다.
+
+// let let;
+// let my school;
+let my_school;      // let mySchool;
+
+let munu = "햄버거";    // 문자열 ' "
+
+let number = 105.123
